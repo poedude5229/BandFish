78b8d3b8a55510cf5ed4e01b2d96089870462752
Joseph <poedude5229@gmail.com>
2024-06-20 12:12:42 -0400

    Fixed small typo

--- app/seeds/wishlists.py
+++ app/seeds/wishlists.py
@@ -14,7 +14,7 @@ def seed_wishlists():
 
 def undo_wishlists():
     if environment == 'production':
-        db.session.execute(f"TRUCATE table {SCHEMA}.wishlists RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.wishlists RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM wishlists"))
     db.session.commit()
